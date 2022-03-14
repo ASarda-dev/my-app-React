61028214f0391d72439ed844608ce423f00bf3f2
Albert <albertsarda89@gmail.com>
2022-03-14 21:29:06 +0000

    Creado props de nombre y edad en un componente reutilizable

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import HolaMundo from "./components/HolaMundo";
-import AdiosMundo from "./components/AdiosMundo";
+import Saludar from "./components/Saludar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HolaMundo />
-        <AdiosMundo />
+        <Saludar name="Albert Sarda Ruiz" edad="32" />
+        <Saludar name="Eva Garcia" edad="34" />
       </header>
     </div>
   );
