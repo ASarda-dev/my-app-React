2268bbbfd001288f8f15fd5677840e2a2fb8a1cd
Albert <albertsarda89@gmail.com>
2022-03-14 18:49:16 +0000

    Creado segundo Componente

---
+++ my-app/src/components/AdiosMundo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdiosMundo() {
+  return (
+    <div>
+      <h1>Adios Mundo</h1>
+      <h2>Segundo componente creado</h2>
+    </div>
+  );
+}
