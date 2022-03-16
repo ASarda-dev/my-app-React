f607d95dcefd41d878ceffaa080c78c420b1eea2
Albert <albertsarda89@gmail.com>
2022-03-16 09:47:45 +0000

    Creados props en comp onente de userInfo y pasaddos a la funcion saludas ()

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,17 @@ import "./App.css";
 import Saludar from "./components/Saludar";
 
 function App() {
+  const user = {
+    nombre: "Albert Sarda",
+    edad: 32,
+    color: "verde",
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Saludar name="Albert Sarda Ruiz" edad="32" />
-        <Saludar name="Eva Garcia" edad="34" />
+        <Saludar userInfo={user} />
+        {/* <Saludar name="Eva Garcia" edad="34" /> */}
       </header>
     </div>
   );
