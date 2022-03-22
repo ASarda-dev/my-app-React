b247800b7d902ab03e4ce37008125e580d602c11
Albert <albertsarda89@gmail.com>
2022-03-22 22:05:04 +0000

    useState config

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,68 +1,25 @@
-import React from "react";
-import { Button, Accordion, Alert, Nav } from "react-bootstrap";
-import { ReactComponent as ReactIcon } from "./assets/react.svg";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+// import { Button, Accordion, Alert, Nav } from "react-bootstrap";
+// import { ReactComponent as ReactIcon } from "./assets/react.svg";
 import "./App.css";
 
 function App() {
+  const [stateCar, setStateCar] = useState(false);
+  const encenderApagar = () => {
+    console.log("Comproba si funciona el boton");
+  };
   return (
     <div className="App">
-      <h1>React Bootstrap </h1>
-      <ReactIcon></ReactIcon>
-      <Nav
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <Button variant="success m-5" size="lg" onClick={console.log("Hola")}>
-        Click
-      </Button>
-      <Alert variant="success">
-        <Alert.Heading>Hey, nice to see you</Alert.Heading>
-        <p>
-          Aww yeah, you successfully read this important alert message. This
-          example text is going to run a bit longer so that you can see how
-          spacing within an alert works with this kind of content.
-        </p>
-        <hr />
-        <p className="mb-0">
-          Whenever you need to, be sure to use margin utilities to keep things
-          nice and tidy.
-        </p>
-      </Alert>
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Abrir</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequ
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Segundo Acordeon</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commo
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+      <h1 class="p-5">React Bootstrap </h1>
+      {/* <ReactIcon></ReactIcon> */}
+
+      <header>
+        <h3>El coche esta : Encendido </h3>
+        <Button variant="success m-5" size="lg" onClick={encenderApagar}>
+          Encender/Apagar
+        </Button>
+      </header>
     </div>
   );
 }
